d14bee9f4dd0eba7f26f6576e8adeab7779399e9
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-07-02 15:22:31 +0200

    add 1 email on array

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-alert('funzia')
+//* Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+const app = new Vue(
+    {
+        el: '#app',
+        data: {
+            mail:[]
+        },
+        mounted(){
+            axios
+                .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(result => {
+                    this.mail.push(result.data.response);
+                    console.log(this.mail);
+                });
+        },
+    }
+);
