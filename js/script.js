f570cca5c80ffd84091a9557c117a32a4ea7c6df
Luca-Riz <77754414+Luca-Riz@users.noreply.github.com>
2021-07-02 15:23:54 +0200

    add 10 email on array

--- js/script.js
+++ js/script.js
@@ -8,12 +8,15 @@ const app = new Vue(
             mail:[]
         },
         mounted(){
-            axios
-                .get('https://flynn.boolean.careers/exercises/api/random/mail')
-                .then(result => {
-                    this.mail.push(result.data.response);
-                    console.log(this.mail);
-                });
+            for(let i = 0; i < 10; i++){
+
+                axios
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then(result => {
+                        this.mail.push(result.data.response);
+                        console.log(this.mail);
+                    });
+            }
         },
     }
 );
